0a784c82723eb6a1e998656d62bbd8c55a0715f7
dipongkor000 <dipongkorroy000@gmail.com>
2025-02-08 22:35:38 +0600

    add file

---
+++ first3.js
@@ -0,0 +1,21 @@
+// loop ---> while condition
+
+// let num = 2;
+// let sum = 0;
+// while (num <= 10){
+//     console.log(num);
+//     sum = sum + num;
+//     console.log("sum =", sum);
+//     num++;
+// }
+
+//even number test
+let num = 1;
+while(num <= 20){
+    console.log(num);
+
+    if(num % 2 === 0){
+        console.log('even number =', num);
+    }
+    num++;
+}
