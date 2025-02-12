43bf5ced1965f11b6b6b6114022fe157d1d968a6
Dipongkor roy <dipongkorroy000@gmail.com>
2025-02-12 14:52:34 +0600

    Update first6.js

--- first6.js
+++ first6.js
@@ -1,35 +1,34 @@
-// odd numbers
+// odd numbers --->
 // for(let i = 0; i < 20; i++){
 //     if(i % 2 === 1){
 //         console.log('this is odd number =',i);
 //     }
 // }
-//alternative rule
+//alternative --
 // for(let i = 0; i < 20; i++){
 //     if(i % 2 !== 0){
 //         console.log('this is odd number from alternative rule =',i);
 //     }
 // }
-//or rules
+//or rules --
 // for(let i = 1; i < 20; i += 2){
 //     console.log('this is odd number =', i);
 // }
 
-// even numbers
+// even numbers --->
 // for(let i = 0; i < 20; i++){
 //     if(i % 2 === 0){
 //         console.log('this is even number =',i);
 //     }
 // }
-//alternative rule
+//alternative rule --
 // for(let i = 0; i < 20; i++){
 //     if(i % 2 !== 1){
 //         console.log('this is even number from alternative rule =', i);
 //     }
 // }
 
-//give me the list numbers between 1 to 30 divisible by 5
-
+//give me the list numbers between 1 to 30 divisible by 5 ---
 // for(let i = 1; i <= 30; i++){
 //     if(i % 5 === 0){
 //         console.log('this number is divisible by 5 =',i);
@@ -48,7 +47,7 @@
 //     }
 // }
 
-// give me the sum of numbers from 1 to 20 that are divisible by 3
+// give me the sum of numbers from 1 to 20 that are divisible by 3 ---
 // let total = 0;
 // for(let i = 1; i <= 20; i++){
 //     if(i % 3 === 0){
