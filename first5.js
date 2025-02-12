f2d2b7d863a4ec71ea924a56bd1508db9d5d1c7d
Dipongkor roy <dipongkorroy000@gmail.com>
2025-02-12 11:49:51 +0600

    Update first5.js

--- first5.js
+++ first5.js
@@ -1,16 +1,15 @@
-//even 
+//even  --->
 // for(let i = 0; i <= 20; i = i + 2){
 //     console.log(i);
 // }
 
-//odd
+//odd --->
 // for(let i = 1; i <= 20; i = i + 2){
 //     console.log(i);
 // }
 
 let sum = 0;
 for(let i = 11; i <= 20; i++){
-    console.log(i);
     sum = sum + i;
 }
 console.log('sum of numbers from 11 to 20 is =', sum);
